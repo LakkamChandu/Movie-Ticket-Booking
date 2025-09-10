5d28eae7c9c1d82fdc78adba3770ae06869a5a25
Chandu <chandhulakkam799@gmail.com>
2025-09-10 20:05:55 +0530

    Handled Exception

--- src/main/java/com/example/movie_ticket/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/movie_ticket/Exception/GlobalExceptionHandler.java
@@ -7,11 +7,14 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFound(ResourceNotFoundException e, WebRequest request){
         Map<String,Object> body = new HashMap<>();
         body.put( "timestamp", LocalDateTime.now() );
@@ -23,8 +26,26 @@ public class GlobalExceptionHandler {
         
     }
 
-    // public ResponseEntity<Object> handleBadRequest(BadRequestException e, WebRequest request){
-    //     return 
-    // }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Object> handleBadRequest(BadRequestException e, WebRequest request){
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "BAD_REQUEST");
+        body.put("message", e.getMessage());
+        body.put("path", request.getDescription(false));
+        return new ResponseEntity<>(body,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralExceptions(Exception e, WebRequest request){
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.put("error", "INTERNAL_SERVER_ERROR");
+        body.put("message", e.getMessage());
+        body.put("path", request.getDescription(false));
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
