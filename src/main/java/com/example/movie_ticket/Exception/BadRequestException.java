239ede634402757d12449ce5b39dd955a90e91bc
Chandu <chandhulakkam799@gmail.com>
2025-09-13 19:48:39 +0530

    Theater branch module only

--- src/main/java/com/example/movie_ticket/Exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.example.movie_ticket.Exception;
-
-public class BadRequestException extends RuntimeException{
-    
-    public BadRequestException(String message){
-        super(message);
-    }
-
-}
--- src/main/java/com/example/movie_ticket/Model/BookingStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.movie_ticket.Model;
-
-public enum BookingStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELLED
-}
